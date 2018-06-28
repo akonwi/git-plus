08074f4acc019406e6b55b4bc475de95994a5a20
akonwi <akonwi@gmail.com>
2018-06-27 23:11:53 -0400

    #218 implement new stash command

--- flow-typed/globals.js
+++ flow-typed/globals.js
@@ -13,15 +13,47 @@ type CommandListeners = {
   [name: string]: CommandListenerFunction
 }
 
+type PanelOptions = {
+  item: HTMLElement | mixed,
+  visible?: boolean,
+  priority?: number,
+  autoFocus?: boolean
+}
+
+type Panel = {
+  destroy(): void
+}
+
+type NotificationButtonOptions = {
+  className?: string,
+  onDidClick?: Function,
+  text: string
+}
+type NotificationOptions = {
+  buttons?: NotificationButtonOptions[],
+  description?: string,
+  detail?: string,
+  dismissable?: boolean,
+  icon?: string
+}
+type ErrorNotificationOptions = NotificationOptions & { stack?: string }
+
+type Notification = {}
+
 type Atom = {
   commands: {
     add(target: string, commandName: string, listener: CommandListener): Disposable,
     add(target: string, commands: CommandListeners): Disposable
   },
+  notifications: {
+    addSuccess(message: string, ?NotificationOptions): Notification,
+    addError(message: string, ?ErrorNotificationOptions): Notification
+  },
   project: {
     repositoryForDirectory(Directory): Promise<GitRepository>
   },
   workspace: {
+    addModalPanel(PanelOptions): Panel,
     getTextEditors(): TextEditor[],
     observeActiveTextEditor(callback: (editor: TextEditor | void) => any): Disposable
   }
