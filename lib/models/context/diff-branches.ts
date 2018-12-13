4fa07d47502f9cb610c94f96a522c8850cc93a23
akonwi <akonwi@gmail.com>
2018-12-13 02:10:15 -0500

    diff branches from tree-view context

---
+++ lib/models/context/diff-branches.ts
@@ -0,0 +1,10 @@
+import Repository from "../../repository";
+import GitDiffBranches = require("../git-diff-branches");
+
+export async function gitDiffBranchesFromContext(treeView: Services.TreeView) {
+  const [path] = treeView.selectedPaths();
+  const repo = await Repository.getForPath(path);
+  if (!repo) return atom.notifications.addWarning(`No repository found for ${path}`);
+
+  GitDiffBranches(repo.repo);
+}
